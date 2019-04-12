0e2108b9b536bbb2cbb68bdaa3fae07e4220261f
Mael Veron <mael.veron@scalian.com>
2019-04-12 17:37:21 +0200

    mise a jour script docker

--- init.ps1
+++ init.ps1
@@ -1,4 +1,3 @@
-docker run -p 5432:5432 --name persistance -e POSTGRES_PASSWORD=123 -e POSTGRES_USER=mael -e POSTGRES_DB=persistance -d postgres
+docker run -p 5432:5432 --name persistance -e POSTGRES_PASSWORD=123 -e POSTGRES_USER=mael -e POSTGRES_DB=persistance -d postgres
 Start-Sleep -Milliseconds 5000
-# exécution des script d'initialisation
-Get-Content .\CompleteScript.sql | docker exec -i persistance  /usr/bin/psql -U mael persistance
+Get-Content .\CompleteScript.sql | docker exec -i persistance /usr/bin/psql -U mael persistance
